b11f236aa0662b6499bf98fab1d17d807377e578
hassanjamila <hassan.jamila@mediacorp.com.sg>
2024-12-14 18:17:06 +0800

    Initial Commit

---
+++ Event CountDown/Event_CountDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  Event_CountDownApp.swift
+//  Event CountDown
+//
+//  Created by Hassan Jamila on 14/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Event_CountDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
