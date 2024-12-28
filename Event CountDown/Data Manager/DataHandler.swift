b5098ed5142d3c9d53b8d28054bdcc0d3ea31887
hassanjamila <hassan.jamila@mediacorp.com.sg>
2024-12-28 13:16:52 +0800

    Remove unnecessary codes

--- Event CountDown/Data Manager/DataHandler.swift
+++ Event CountDown/Data Manager/DataHandler.swift
@@ -8,50 +8,6 @@
 import Foundation
 
 final class DataHandler {
-    private static var allEvents: [EventModel] = []
-    
-    /**
-        Used to add new event
-        Parameter: An event to be inserted
-     */
-    static func addEvent(event: EventModel) {
-        allEvents.append(event)
-    }
-    
-    /**
-        Used to update an event
-        id: The id of the event that you want to update
-        newEventData: The updated event
-     */
-    static func updateEvent(id: UUID, newEventData: EventModel) {
-        if let index = allEvents.firstIndex(where: { $0.id == id }) {
-            allEvents[index] = newEventData
-        }
-    }
-    
-    /**
-        Used to remove an event
-        id: The id of the event that you want to remove
-     */
-    static func removeEventByID(id: UUID) {
-        allEvents.removeAll { event in
-            event.id == id
-        }
-    }
-    
-    /**
-        Used to get all the events
-        Return: An array of events
-     */
-    static func getAllEvents() -> [EventModel] {
-        return allEvents
-    }
-    
-    /// Used in the unit tests to reset the events
-    static func resetAllEvents() {
-        allEvents = []
-    }
-    
     static let SAMPLE_DATA = [
         EventModel(eventTitle: "Event From 5 days", evnetDate: Calendar.current.date(byAdding: .day, value: -5, to: Date())!, eventTextColor: .blue),
         EventModel(eventTitle: "Event From 2 Days", evnetDate: Calendar.current.date(byAdding: .day, value: -2, to: Date())!, eventTextColor: .green),
